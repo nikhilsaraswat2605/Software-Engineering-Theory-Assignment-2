fa0135a5e4ecffb0e761ef4a9ae03c8ce65fd215
Nikhil <nikhilsaraswat2605@gmail.com>
2022-02-11 15:31:48 +0530

    commented

--- P2.cpp
+++ P2.cpp
@@ -30,17 +30,17 @@ public:
         return is;
     }
 };
-// customer class
+// ProductItem class
 class ProductItem
 {
     string title;
     int id;
-    static int NumProductItem;// just for uniqueness of ProductItem IDs
+    static int NumProductItem; // just for uniqueness of ProductItem IDs
     float price;
     int copies;
 
 public:
-    ProductItem(string title = "NA", float price = 0)// constructor
+    ProductItem(string title = "NA", float price = 0) // constructor
     {
         this->title = title;
         this->price = price;
@@ -49,7 +49,7 @@ public:
     }
     // destructor
     ~ProductItem() {}
-    friend ostream &operator<<(ostream &os, const ProductItem &pi)// output stream
+    friend ostream &operator<<(ostream &os, const ProductItem &pi) // output stream
     {
         os << "Product id: " << pi.id << ", "
            << "Produt title: " << pi.title << ", "
@@ -57,17 +57,17 @@ public:
            << "Produt copies: " << pi.copies << "\n";
         return os;
     }
-    friend istream &operator>>(istream &is, ProductItem &pi)
+    friend istream &operator>>(istream &is, ProductItem &pi) // input stream
     {
         is >> pi.title >> pi.price >> pi.copies;
         return is;
     }
-    ProductItem &operator*(int a)
+    ProductItem &operator*(int a) // overloading of * operator, here we will add a in number of copies of object
     {
         this->copies += a;
         return *this;
     }
-    ProductItem &operator=(const ProductItem &pi)
+    ProductItem &operator=(const ProductItem &pi) // overloading of = operator
     {
         this->title = pi.title;
         this->id = pi.id;
@@ -76,41 +76,43 @@ public:
         return *this;
     }
 };
+// Order class
 class Order
 {
     int id;
-    static int NumOrder;
+    static int NumOrder; // just for uniqueness of Order IDs
     Customer c;
     vector<ProductItem> prods;
 
 public:
-    Order(Customer &in_c)
+    Order(Customer &in_c) // constructor
     {
         this->c = in_c;
         this->id = ++NumOrder;
     }
+    // destructor
     ~Order() {}
-    int getid()
+    int getid() // for accessing ID of the object
     {
         return this->id;
     }
-    friend ostream &operator<<(ostream &os, const Order &o)
+    friend ostream &operator<<(ostream &os, const Order &o) // output stream
     {
         os << "Order id: " << o.id << ", "
            << "Customer-> " << o.c << "\n";
         return os;
     }
-    friend istream &operator>>(istream &is, Order &o)
+    friend istream &operator>>(istream &is, Order &o) // input stream
     {
         is >> o.c;
         return is;
     }
-    Order &operator+(ProductItem &p)
+    Order &operator+(ProductItem &p) // overloading of + operator, here we will add productItem in the Order object
     {
         this->prods.push_back(p);
         return *this;
     }
-    Order &operator=(const Order &o)
+    Order &operator=(const Order &o) // overloading of = operator
     {
         this->id = o.id;
         this->c = o.c;
@@ -118,37 +120,39 @@ public:
         return *this;
     }
 };
+// ShoppingBasket class
 class ShoppingBasket
 {
     int id;
-    static int NumBasket;
+    static int NumBasket; // just for uniqueness of NumBasket IDs
     Customer c;
     list<Order> orders;
 
 public:
-    ShoppingBasket(Customer &in_c)
+    ShoppingBasket(Customer &in_c) // constructor
     {
         this->c = in_c;
         this->id = ++NumBasket;
     }
+    // destructor
     ~ShoppingBasket() {}
-    friend ostream &operator<<(ostream &os, const ShoppingBasket &sb)
+    friend ostream &operator<<(ostream &os, const ShoppingBasket &sb) // output stream
     {
         os << "Shop id: " << sb.id << ", "
            << "Customer-> " << sb.c << "\n";
         return os;
     }
-    friend istream &operator>>(istream &is, ShoppingBasket &sb)
+    friend istream &operator>>(istream &is, ShoppingBasket &sb) // input stream
     {
         is >> sb.c;
         return is;
     }
-    ShoppingBasket &operator+(Order &p)
+    ShoppingBasket &operator+(Order &p) // overloading of + operator, here we will add productItem in the ShoppingBasket object
     {
         this->orders.push_back(p);
         return *this;
     }
-    ShoppingBasket &operator-(int orderid)
+    ShoppingBasket &operator-(int orderid) // overloading of - operator, here we shall remove the order of particular ID from a ShoppingBasket Object
     {
         auto itr = this->orders.begin();
         for (; itr != this->orders.end(); itr++)
@@ -162,7 +166,7 @@ public:
         }
         return *this;
     }
-    ShoppingBasket &operator=(ShoppingBasket const &sb)
+    ShoppingBasket &operator=(ShoppingBasket const &sb) // overloading of = operator
     {
         this->id = sb.id;
         this->c = sb.c;
@@ -170,10 +174,12 @@ public:
         return *this;
     }
 };
+// initializing the static variables with 0
 int Customer::NumCustomer = 0;
 int ProductItem::NumProductItem = 0;
 int Order::NumOrder = 0;
 int ShoppingBasket::NumBasket = 0;
+
 int main()
 {
     // create a customer
